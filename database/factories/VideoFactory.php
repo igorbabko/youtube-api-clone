95e3b3331168994c2fd844e842d5fd56caf9a1cd
Igor Babko <i.i.babko@gmail.com>
2022-07-31 00:40:27 +0300

    22: Seed random dates for videos

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -22,9 +22,23 @@ class VideoFactory extends Factory
      */
     public function definition()
     {
+        $createdAt = $this->faker->dateTimeThisYear();
+        // $createdAt = $this->faker->dateTimeThisMonth();
+        // $createdAt = $this->faker->dayOfWeek();
+        // $createdAt = $this->faker->dateTimeBetween('-2 months', '+1 week');
+        // $createdAt = $this->faker->dateTimeInInterval('-1 week', '+3 days');
+
+        // $createdAt = $this->faker->dateTimeBetween('-1 year');
+        // $createdAt = $this->faker->dateTimeBetween('-1 month');
+        // $createdAt = $this->faker->dateTimeBetween('-1 week');
+        // $createdAt = $this->faker->dateTimeBetween('-1 day');
+        // $createdAt = $this->faker->dateTimeBetween('-1 hour');
+
         return [
             'title' => ucfirst($this->faker->words(mt_rand(1, 2), true)),
             'channel_id' => Channel::inRandomOrder()->first(),
+            'created_at' => $createdAt,
+            'updated_at' => $createdAt,
         ];
     }
 }
