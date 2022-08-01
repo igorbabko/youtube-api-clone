46337c8736cbccc8f130c7cd331c0db4e506a034
Igor Babko <i.i.babko@gmail.com>
2022-08-02 01:17:26 +0300

    24: Filter videos by period

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -8,13 +8,18 @@ class VideoController extends Controller
 {
     public function index()
     {
-        return [
-            'year' => Video::where('created_at', '>=', now()->startOfYear())->get(),
-            'month' => Video::where('created_at', '>=', now()->startOfMonth())->get(),
-            'week' => Video::where('created_at', '>=', now()->startOfWeek())->get(),
-            'day' => Video::where('created_at', '>=', now()->startOfDay())->get(),
-            'hour' => Video::where('created_at', '>=', now()->startOfHour())->get(),
-        ];
+        switch (request('period')) {
+            case 'year': $date = now()->startOfYear(); break;
+            case 'month': $date = now()->startOfMonth(); break;
+            case 'week': $date = now()->startOfWeek(); break;
+            case 'day': $date = now()->startOfDay(); break;
+            case 'hour': $date = now()->startOfHour(); break;
+            default: $date = null;
+        }
+
+        return $date
+            ? Video::where('created_at', '>=', $date)->get()
+            : Video::get();
     }
 
     public function show(Video $video)
