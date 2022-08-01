201f9c9d2b76386db5faca2e527baace33c9f77b
Igor Babko <i.i.babko@studnote.com>
2022-08-01 16:21:45 +0300

    22 - createdAt()

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -8,14 +8,13 @@ class VideoController extends Controller
 {
     public function index()
     {
-        return Video::with('channel', 'categories')
-          ->where('created_at', '>=', now()->startOfYear())
-          // ->where('created_at', '>=', now()->startOfMonth())
-          // ->where('created_at', '>=', now()->startOfWeek())
-          // ->where('created_at', '>=', now()->startOfDay())
-          // ->where('created_at', '>=', today())
-          // ->where('created_at', '>=', now()->startOfHour())
-          ->count();
+        return [
+            'year' => Video::where('created_at', '>=', now()->startOfYear())->get(),
+            'month' => Video::where('created_at', '>=', now()->startOfMonth())->get(),
+            'week' => Video::where('created_at', '>=', now()->startOfWeek())->get(),
+            'day' => Video::where('created_at', '>=', now()->startOfDay())->get(),
+            'hour' => Video::where('created_at', '>=', now()->startOfHour())->get(),
+        ];
     }
 
     public function show(Video $video)
