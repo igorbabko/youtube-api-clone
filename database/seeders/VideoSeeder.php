79e6603a9e7010926fbd4766a090ab50c635fa2b
Igor Babko <i.i.babko@studnote.com>
2022-08-07 00:53:09 +0300

    28 - use factory state for period

--- database/seeders/VideoSeeder.php
+++ database/seeders/VideoSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Period;
 use App\Models\Video;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,13 @@ class VideoSeeder extends Seeder
      */
     public function run()
     {
-        Video::factory(100)->create();
+        collect(Period::cases())
+          ->each(fn (Period $period) => Video::factory(20)->last($period)->create());
+
+        // Video::factory(20)->last(Period::Year)->create();
+        // Video::factory(20)->last(Period::Month)->create();
+        // Video::factory(20)->last(Period::Week)->create();
+        // Video::factory(20)->last(Period::Day)->create();
+        // Video::factory(20)->last(Period::Hour)->create();
     }
 }
