9edf0ac86ea8cb8d3845c5f3ab82320c775151f4
Igor Babko <i.i.babko@studnote.com>
2022-08-08 00:57:26 +0300

    29 - with

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -11,18 +11,16 @@ class VideoController extends Controller
     {
         $period = Period::tryFrom(request('period'));
 
-        return Video::fromPeriod($period)
+        return Video::with(request('with', []))
+          ->fromPeriod($period)
           ->search(request('query'))
           ->limit(request('limit'))
-          // ->take(request('limit))
           ->orderBy(request('sort', 'created_at'), request('order', 'asc'))
-          // ->oldest()
-          // ->dd()
           ->get();
     }
 
     public function show(Video $video)
     {
-        return $video->load('channel', 'categories');
+        return $video->load(request('with', []));
     }
 }
