1d7d507a71d32383d771de74d526cb813bfdfb3b
Igor Babko <i.i.babko@gmail.com>
2022-08-15 10:24:48 +0300

    36: Filter, sort and paginate users.index request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,11 +8,14 @@ class UserController extends Controller
 {
     public function index()
     {
-        return User::with('channel')->get();
+        return User::with(request('with', []))
+            ->search(request('query'))
+            ->orderBy(request('sort', 'name'), request('order', 'asc'))
+            ->simplePaginate(request('limit'));
     }
 
     public function show(User $user)
     {
-        return $user->load('channel');
+        return $user->load(request('with', []));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,12 @@ class User extends Authenticatable
     {
         return $this->hasOne(Channel::class);
     }
+
+    public function scopeSearch($query, ?string $text)
+    {
+        return $query->where(function ($query) use ($text) {
+            $query->where('name', 'like', "%$text%")
+                ->orWhere('email', 'like', "%$text%");
+        });
+    }
 }
