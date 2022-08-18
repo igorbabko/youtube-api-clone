9cd80c8d2c13769576a963c6f396b67e6db68000
Igor Babko <i.i.babko@studnote.com>
2022-08-18 11:44:39 +0300

    41 - refactor PlaylistVideoSeeder

--- database/seeders/PlaylistVideoSeeder.php
+++ database/seeders/PlaylistVideoSeeder.php
@@ -6,36 +6,20 @@ use App\Models\Channel;
 use App\Models\Playlist;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\DB;
 
 class PlaylistVideoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        $playlists = Playlist::all();
-
-        $playlistVideos = $playlists->flatMap(
-          fn ($playlist) => $this->playlistVideos($playlist, $this->randomVideosFrom($playlist->channel))
-        );
-
-        DB::table('playlist_video')->insert($playlistVideos->all());
-    }
-
-    private function playlistVideos(Playlist $playlist, Collection $videos): Collection
-    {
-        return $videos->map(fn ($video) => [
-            'playlist_id' => $playlist->id,
-            'video_id' => $video->id,
-        ]);
+        Playlist::all()
+          ->each(fn (Playlist $playlist) => $playlist->videos()->saveMany($this->randomVideosFrom($playlist->channel)));
     }
 
     private function randomVideosFrom(Channel $channel): Collection
     {
-        return $channel->videos->random(mt_rand(1, count($channel->videos)));
+        return $channel->videos->whenEmpty(
+            fn () => collect(),
+            fn ($videos) => $videos->random(mt_rand(1, $videos->count()))
+        );
     }
 }
