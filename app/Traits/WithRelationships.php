76ba551702e92e759c5ac603d254c9ff6966c17f
Igor Babko <i.i.babko@gmail.com>
2022-08-28 11:40:28 +0300

    48: Improve WithRelationships trait

--- app/Traits/WithRelationships.php
+++ app/Traits/WithRelationships.php
@@ -2,12 +2,16 @@
 
 namespace App\Traits;
 
-use Illuminate\Support\Arr;
-
 trait WithRelationships
 {
-    public function scopeWithRelationships($query, array|string $with)
+    public function scopeWithRelationships($query, array|string $relationships)
     {
-        return $query->with(array_intersect(Arr::wrap($with), static::$relationships ?? []));
+        $validRelationships = collect($relationships)
+            ->map(fn (string $relationships): array => explode('.', $relationships))
+            ->filter(fn (array $relationships): bool => in_array($relationships[0], static::$relationships))
+            ->map(fn (array $relationships): string => implode('.', $relationships))
+            ->all();
+
+        return $query->with($validRelationships);
     }
 }
