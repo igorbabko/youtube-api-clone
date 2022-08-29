c5cf366f89a3d9fed97568583e94da5c76b0beb3
Igor Babko <i.i.babko@studnote.com>
2022-08-29 10:20:24 +0300

    48 - nested relationships refactor

--- app/Traits/WithRelationships.php
+++ app/Traits/WithRelationships.php
@@ -14,12 +14,28 @@ trait WithRelationships
         return $this->load(static::validRelationships($relationships));
     }
 
-    private static function validRelationships($relationships): array
+    public static function validRelationships($relationships)
     {
         return collect($relationships)
             ->map(fn (string $relationships): array => explode('.', $relationships))
-            ->filter(fn (array $relationships): bool => in_array($relationships[0], static::$relationships))
+            ->filter(fn (array $relationships): bool => (new static)->hasRelationships($relationships))
             ->map(fn (array $relationship): string => implode('.', $relationship))
             ->all();
+    }
+
+    public function hasRelationships(array $relationships)
+    {
+        return (bool) collect($relationships)
+            ->reduce(fn ($model, $relationship) => optional($model)->hasRelationship($relationship), $this);
+    }
+
+    public function hasRelationship(string $relationship)
+    {
+        return $this->isValidRelationship($relationship) ? $this->$relationship()->getRelated() : null;
+    }
+
+    public function isValidRelationship(string $relationship)
+    {
+        return method_exists($this, $relationship) && in_array($relationship, static::$relationships);
     }
 }
