e730bfec8ecf6c16610c27f2c8328f1b423775c5
Igor Babko <i.i.babko@gmail.com>
2022-08-30 11:34:10 +0300

    50: Refactor WithRelationships trait

--- app/Traits/WithRelationships.php
+++ app/Traits/WithRelationships.php
@@ -8,18 +8,26 @@ trait WithRelationships
     {
         $validRelationships = collect($relationships)
             ->map(fn (string $relationships): array => explode('.', $relationships))
-            ->filter(function (array $relationships) {
-                return collect($relationships)->reduce(function ($model, $relationship) {
-                    if ($model && method_exists($model, $relationship) && in_array($relationship, $model::$relationships)) {
-                        return $model->$relationship()->getRelated();
-                    }
-
-                    return null;
-                }, new static);
-            })
+            ->filter(fn (array $relationships): bool => (new static)->hasRelationships($relationships))
             ->map(fn (array $relationships): string => implode('.', $relationships))
             ->all();
 
         return $query->with($validRelationships);
+    }
+
+    public function hasRelationships(array $relationships)
+    {
+        return (bool) collect($relationships)
+            ->reduce(fn ($model, $relationship) => optional($model)->hasRelationship($relationship), $this);
+    }
+
+    public function hasRelationship(string $relationship)
+    {
+        return $this->isValidRelationship($relationship) ? $this->$relationship()->getRelated() : null;
+    }
+
+    public function isValidRelationship(string $relationship)
+    {
+        return method_exists($this, $relationship) && in_array($relationship, static::$relationships);
     }
 }
