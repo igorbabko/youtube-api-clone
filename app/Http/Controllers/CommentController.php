843e08273004cab86c9cc421dec52a017fda16cb
Igor Babko <i.i.babko@gmail.com>
2023-02-16 16:13:41 +0200

    61: comments - refactor

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -26,34 +26,29 @@ class CommentController extends Controller
             'video_id' => 'required_without:parent_id|exists:videos,id',
         ]);
 
-        $attributes['user_id'] = $request->user()->id;
-
-        if ($request->parent_id) {
-            $attributes['video_id'] = Comment::find($request->parent_id)->video_id;
-        }
-
         return Comment::create($attributes);
     }
 
     public function update(Comment $comment, Request $request)
     {
-        throw_if($request->user()->isNot($comment->user), AuthorizationException::class);
+        $this->checkPermissions($comment, $request);
 
         $attributes = $request->validate([
             'text' => 'required|string',
         ]);
 
         $comment->fill($attributes)->save();
-
-        return $comment;
     }
 
     public function destroy(Comment $comment, Request $request)
     {
-        throw_if($request->user()->isNot($comment->user), AuthorizationException::class);
-
-        $comment->replies()->delete();
+        $this->checkPermissions($comment, $request);
 
         $comment->delete();
+    }
+
+    private function checkPermissions(Comment $comment, Request $request)
+    {
+        throw_if($request->user()->isNot($comment->user), AuthorizationException::class);
     }
 }
