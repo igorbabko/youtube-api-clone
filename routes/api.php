8f0c889b1b2497d78f1f954d097c8d5a118b35b0
Igor Babko <i.i.babko@gmail.com>
2023-02-19 17:50:38 +0200

    x: controller and route for storing tokens

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ChannelController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PersonalAccessTokenController;
 use App\Http\Controllers\PlaylistController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VideoController;
@@ -28,3 +29,5 @@ Route::get('/comments/{comment}', [CommentController::class, 'show']);
 Route::post('/comments', [CommentController::class, 'store']);
 Route::put('/comments/{comment}', [CommentController::class, 'update']);
 Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
+
+Route::post('/personal-access-tokens', [PersonalAccessTokenController::class, 'store']);
