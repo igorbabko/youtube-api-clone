1d6a719e7fac9f72968951598e702cecd23f0d2a
Igor Babko <i.i.babko@gmail.com>
2023-03-09 13:14:23 +0200

    76: Login

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return response($request->user(), Response::HTTP_CREATED);
+        }
+
+        return response([
+            'email' => 'The provided credentials do not match our records.',
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
